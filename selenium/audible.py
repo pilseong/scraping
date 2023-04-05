f4d4baa44fea7542212747eb7d0b2f4aed311133
pilseong <heops79@gmail.com>
2023-04-05 19:08:04 +0900

    update

--- selenium/audible.py
+++ selenium/audible.py
@@ -1,21 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'
-# path = '/mnt/c/Users/heops/Downloads/chromedriver_win32/chromedriver'
 path = '/home/pilseong/chromedriver_linux64/chromedriver'
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
+chrome_options.headless = True
+chrome_options.add_argument('window-size=1920x1080')
 
-# user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.49 Safari/537.36'
-# chrome_options.add_argument(f'user-agent={user_agent}')
-# chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-infobars')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--remote-debugging-port=9222')
 driver = webdriver.Chrome(path, options=chrome_options)
 driver.get(website)
 
