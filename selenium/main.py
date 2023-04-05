0b0ece3ce20f4a14bd86e6663f20ed52c3ae3781
pilseong <heops79@gmail.com>
2023-04-05 13:26:47 +0900

    selenium update

--- selenium/main.py
+++ selenium/main.py
@@ -3,14 +3,17 @@ from selenium.webdriver.common.by import By
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'
 # path = '/mnt/c/Users/heops/Downloads/chromedriver_win32/chromedriver'
-path = '/home/heops/chromedriver_linux64/chromedriver'
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--disable-infobars')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--no-sandbox')
-options.add_argument('--remote-debugging-port=9222')
-driver = webdriver.Chrome(path, options=options)
+path = '/home/pilseong/chromedriver_linux64/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.49 Safari/537.36'
+chrome_options.add_argument(f'user-agent={user_agent}')
+chrome_options.add_argument('headless')
+chrome_options.add_argument('--disable-infobars')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--remote-debugging-port=9222')
+driver = webdriver.Chrome(path, options=chrome_options)
 driver.get(website)
 
 all_matches_btn = driver.find_element(
